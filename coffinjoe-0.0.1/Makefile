77ba7dbd8e06d1e6d1d5a55da838d129f2534c53
vitor0 <64447170+vitor0@users.noreply.github.com>
2022-03-19 16:30:40 -0300

    Add files via upload

---
+++ coffinjoe-0.0.1/Makefile
@@ -0,0 +1,13 @@
+# Setup name variables for the package/tool
+NAME := coffinjoe
+PKG := github.com/heltonmarx/$(NAME)
+
+CGO_ENABLED := 0
+
+# Set any default go build tags.
+BUILDTAGS :=
+
+include basic.mk
+
+.PHONY: prebuild
+prebuild:
---
+++ coffinjoe-0.0.1/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/genuinetools/pkg/cli"
+	"github.com/heltonmarx/coffinjoe/version"
+)
+
+var (
+	username string
+	password string
+	date     string
+	output   string
+)
+
+const host = "https://selo.tjsc.jus.br/selo/CertidaoService"	//"https://selo.tjsc.jus.br/selo_teste/CertidaoService" --> HML
+
+func main() {
+	p := cli.NewProgram()
+	p.Description = "API client to consume the death certificate from tjsc"
+	p.FlagSet = flag.NewFlagSet("global", flag.ExitOnError)
+
+	p.GitCommit = version.GITCOMMIT
+	p.Version = version.VERSION
+
+	//p.FlagSet.StringVar(&username, "username", "convenio", "TJ-SC username")
+	//p.FlagSet.StringVar(&username, "u", "convenio", "TJ-SC username")
+	p.FlagSet.StringVar(&username, "username", "convenio_cge", "TJ-SC username")
+	p.FlagSet.StringVar(&username, "u", "convenio_cge", "TJ-SC username")	
+
+//	p.FlagSet.StringVar(&password, "password", "selodigital", "TJ-SC password")
+//	p.FlagSet.StringVar(&password, "p", "selodigital", "TJ-SC password")
+	p.FlagSet.StringVar(&password, "password", "myp1y2MOds", "TJ-SC password")
+	p.FlagSet.StringVar(&password, "p", "myp1y2MOds", "TJ-SC password")
+
+
+	p.FlagSet.StringVar(&date, "date", "2018-06-04", "specific date of request (YYYY-MM-DD)")
+	p.FlagSet.StringVar(&date, "d", "2018-06-04", "specific date of request (YYYY-MM-DD)")
+
+	p.FlagSet.StringVar(&output, "output", "certificates", "Output certificates JSON file")
+	p.FlagSet.StringVar(&output, "o", "certificates", "Output certificates JSON file")
+
+	p.Before = func(ctx context.Context) error {
+		signals := make(chan os.Signal)
+		signal.Notify(signals, os.Interrupt)
+		signal.Notify(signals, syscall.SIGTERM)
+		_, cancel := context.WithCancel(ctx)
+		go func() {
+			for sig := range signals {
+				cancel()
+				log.Printf("Received %s, exiting", sig.String())
+				os.Exit(0)
+			}
+		}()
+		return nil
+	}
+	p.Action = func(ctx context.Context, args []string) error {
+		client := NewClient(host, WithCredentials(username, password))
+		cd, err := client.GetDeatchCertificateByDate(ctx, date)
+		if err != nil {
+			return err
+		}
+		if err := exportJSON(cd, output); err != nil {
+			return err
+		}
+		return exportXML(cd, output)
+	}
+	p.Run()
+}
